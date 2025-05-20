27102fc231ef106df0d4b5ef819a57893538d5a4
Bhoomika-Ramanaik <bhoomi.r1230@gmail.com>
2025-05-20 22:27:20 +0530

    Add files via upload

---
+++ 1b.ArrayList_Programs/SubListColors.java
@@ -0,0 +1,14 @@
+package Search;
+
+import java.util.*;
+
+public class SubListColors {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>(Arrays.asList("Red", "Green", "Blue", "Yellow"));
+        System.out.println("Original Colors: " + colors);
+
+        List<String> subColors = colors.subList(0, 2); // index 0 and 1
+        System.out.println("Extracted Elements (1st and 2nd): " + subColors);
+    }
+}
+
