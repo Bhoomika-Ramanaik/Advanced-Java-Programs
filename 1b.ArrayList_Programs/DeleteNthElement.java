748e9a90752e014c70fd20f0c217e5792256b3c6
Bhoomika-Ramanaik <bhoomi.r1230@gmail.com>
2025-05-20 22:28:33 +0530

    Add files via upload

---
+++ 1b.ArrayList_Programs/DeleteNthElement.java
@@ -0,0 +1,19 @@
+package Search;
+
+import java.util.*;
+
+public class DeleteNthElement {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>(Arrays.asList("Red", "Green", "Blue", "Yellow", "Pink"));
+        System.out.println("Original Colors: " + colors);
+
+        int n = 3; // For example, deleting 3rd element (index 2)
+        if (n - 1 >= 0 && n - 1 < colors.size()) {
+            colors.remove(n - 1);
+            System.out.println("After deleting " + n + "rd element: " + colors);
+        } else {
+            System.out.println("Invalid index: " + (n - 1));
+        }
+    }
+}
+
